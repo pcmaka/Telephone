050591dd299e302b44909f601fc8fcf72ab9ff13
Max Klein <max.klein@4mk.eu>
2017-07-03 22:09:33 +0200

    Revert "Fixed Build Errors"
    This reverts commit ba50fe30ceb75c1069ab24ea1fb24523600db21d.

--- Telephone/ReceiptXPCGateway.swift
+++ Telephone/ReceiptXPCGateway.swift
@@ -24,7 +24,7 @@ final class ReceiptXPCGateway {
     fileprivate let connection: NSXPCConnection
 
     init() {
-        connection = NSXPCConnection(serviceName: "eu.4mk.Telephone.ReceiptValidation")
+        connection = NSXPCConnection(serviceName: "com.tlphn.Telephone.ReceiptValidation")
         connection.remoteObjectInterface = NSXPCInterface(with: ReceiptValidation.self)
         connection.resume()
     }
